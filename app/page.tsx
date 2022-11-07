0831d7d4fd28f1a167b73bee31a8e93379eef4a6
Crow <garretthugs@gmail.com>
2022-11-07 17:53:54 -0500

    updated title

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="container mx-auto">
       <main>
-        <h1 className="text-center text-2xl">Next.JS 13 Playground</h1>
+        <h1 className="text-center text-2xl">Next.JS 13 Reference</h1>
         <pre className="bg-gray-900 max-w-xl mx-auto p-4 overflow-x-auto">
           <code>
             <RunCommand>npx</RunCommand> create-next-app@latest
